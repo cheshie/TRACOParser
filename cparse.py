eacbd7d566c4e3e97b726dab23bdb21d574aee62
Przemyslaw Samsel <sp39427@zut.edu.pl>
2020-04-02 23:27:12 +0200

    A few comments

--- cparse.py
+++ cparse.py
@@ -11,7 +11,6 @@ def parse_arguments(args_parser = ArgumentParser(prog='PLUTO / TRACO Code Parser
     args_parser.add_argument('-i', '--infile', help='input file name', default='example.c')
     args_parser.add_argument('-o', help='output file')
 
-    # Return parsed arguments and a list containing dictionaries for separate groups
     return args_parser.parse_args()
 #
 
@@ -19,6 +18,8 @@ class Constructions():
     def __init__(self, name):
         if name == 'for':                # tuple name cannot be known python keyword!
             self.Constr    = namedtuple('for_n', ['init', 'end_condition', 'increment', 'instruction'])
+                                                                            # TODO: parallel for's should be special-marked!
+            # TODO: Special case: what if incrementation is outside???
             self.method    = self.eval_for
         if name =='pragma':
             self.Constr    =  namedtuple('pragma', ['text', 'instruction'])
@@ -28,18 +29,27 @@ class Constructions():
     #
 
     def eval_for(self, instruction):
+        # TODO: For special cases - not handled yet!
         self.Constr.init, self.Constr.end_condition, self.Constr.increment = \
             search(r'.*?\((.*)\).*', instruction).group(1).split(';')
+        # Case 1: for (init ; end_condition ; increment ) => handled
+        # Case 2: for ( ; ; ) => not handled
+        # Case 3: for (init; end_condition; ) => not handled
+        # Case 4: for (;end_condition; increment) => not handled
+        # Case 5: for (;; increment) => not handled
+        #                ^ are these semicolons needed?
+        # Case 6: (special) i.e. for ( int i, j; i < 5,j>3; i++,j++ )
     #
 
     def eval_pragma(self, instruction):
-        self.Constr.text = instruction
+        pass
+        #self.Constr.text = instruction
     #
 #
 
 class Parser():
     def __init__(self, input):
-        self.in_fname   = input
+        self.in_fname   = input #CHANGE THIS NAME - python keyword
         self.f_contents = None
         self.file_structure = {'include': None, 'variables': None, 'instructions' : None}
         self.keywords       = {'for', 'pragma'}
