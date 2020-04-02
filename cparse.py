87a928cf9f2f459c96d5856148c86b5b633cfcb8
Przemyslaw Samsel <sp39427@zut.edu.pl>
2020-04-02 20:51:00 +0200

    Parsing file in a simple way, getting all instructions into python structures, simple re for now

--- cparse.py
+++ cparse.py
@@ -1,7 +1,11 @@
 from argparse import ArgumentParser
-from _collections import namedtuple
+from collections import namedtuple, deque
 
 # Parse command line arguments
+from re import search, compile
+import re
+
+
 def parse_arguments(args_parser = ArgumentParser(prog='PLUTO / TRACO Code Parser',
                                                  description='Parsing code from P/T to CUDA')):
     args_parser.add_argument('-i', '--infile', help='input file name', default='example.c')
@@ -11,22 +15,63 @@ def parse_arguments(args_parser = ArgumentParser(prog='PLUTO / TRACO Code Parser
     return args_parser.parse_args()
 #
 
+class Constructions():
+    def __init__(self, name):
+        if name == 'for':                # tuple name cannot be known python keyword!
+            self.Constr    = namedtuple('for_n', ['init', 'end_condition', 'increment', 'instruction'])
+            self.method    = self.eval_for
+        if name =='pragma':
+            self.Constr    =  namedtuple('pragma', ['text', 'instruction'])
+            self.method    = self.eval_pragma
+        if name =='variable':
+            self.Constr = namedtuple('var', ['name', 'value', 'size', 'type'])
+    #
+
+    def eval_for(self, instruction):
+        self.Constr.init, self.Constr.end_condition, self.Constr.increment = \
+            search(r'.*?\((.*)\).*', instruction).group(1).split(';')
+    #
+
+    def eval_pragma(self, instruction):
+        self.Constr.text = instruction
+    #
+#
+
 class Parser():
     def __init__(self, input):
         self.in_fname   = input
         self.f_contents = None
+        self.file_structure = {'include': None, 'variables': None, 'instructions' : None}
+        self.keywords       = {'for', 'pragma'}
     #
 
     def readfile(self):
         with open(self.in_fname, 'r') as lines:
-            self.f_contents = lines
-            for x in self.f_contents:
-                print(x)
+            # Read contents of the file
+            self.f_contents = deque(list(lines)[1:]) # LEAVE 1st LINE (pragma) for now!
+            for line in self.f_contents:
+                # If empty line spotted, skip a line
+                if search(r'^$', line):
+                    continue
+
+                # Read first word in line, prepended by # or not
+                instruction = search(r'^(\s*)[#]{0,1}(\w+)', line).group(1)
+
+                # Check if its in keywords
+                if instruction in self.keywords:
+                    # Create object containing code in the line
+                    ins_struct = Constructions(instruction)
+                    self.file_structure['instructions'] = ins_struct
+                    self.file_structure['instructions'].method(line.replace(instruction, ''))
+                else:
+                    # Assign variable here
+                    pass
     #
 
-    # Which on of this methods is used to print etc?
+    # To print class into a readable format
     def __str__(self):
-        return "what"
+        # ALL VARIABLES AND INSTRUCTIONS IN JSON FORMAT HER???
+        return "pass"
 #
 
 if __name__ == '__main__':
