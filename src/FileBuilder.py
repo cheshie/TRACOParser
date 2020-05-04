3b6775b9418ffa88f14d32d3c2fb56eb785491a5
Paweł Kalicki <kalicki.paul@icloud.com>
2020-05-04 19:41:12 +0200

    Reformat file with PEP8

--- src/FileBuilder.py
+++ src/FileBuilder.py
@@ -1,16 +1,17 @@
 import os
-import json
 import logging
+from typing import TextIO
+
 from Parser.Constructions import Constructions
 
 logger = logging.getLogger(__name__)
 
 _ext = '.cu'
 
-lib_folder = os.getcwd()+'/libs'
+lib_folder = os.getcwd() + '/libs'
 if not os.path.exists(lib_folder):
     os.makedirs(lib_folder)
-prod_folder = os.getcwd()+'/Products'
+prod_folder = os.getcwd() + '/Products'
 if not os.path.exists(prod_folder):
     os.makedirs(prod_folder)
 
@@ -39,11 +40,12 @@ values = resources[constFiles[6]]
 
 
 class FileBuilder:
+    file: TextIO
+
     def __init__(self, phrase, filename):
         logger.info('Initialized FileBuilder')
         self.phrase = phrase
         self.make_file(filename)
-        self.file
 
     def make_file(self, filename):
         self.file = open(
@@ -57,12 +59,13 @@ class FileBuilder:
         if self.phrase.variables:
             for var in self.phrase.variables:
                 if type(self.phrase.variables[var]) is not dict:
-                    logger.warn('var: {} is {} '.format(
+                    logger.warning('var: {} is {} '.format(
                         var, type(self.phrase.variables[var]), ))
                 if type(self.phrase.variables[var]) is dict:
-                    logger.warn('{} is matrix'.format(var))
+                    logger.warning('{} is matrix'.format(var))
         else:
-            logger.warn('no variables')
+            logger.warning('no variables')
+
     # file.phrase.instructions[1].Constr.instructions[0].Constr.instructions[0]
     @staticmethod
     def check_variables_type2(variables):
@@ -70,10 +73,10 @@ class FileBuilder:
         if variables:
             for var in variables:
                 if type(variables[var]) is not dict:
-                    logger.warn('var: {} is {} type --> {}'.format(
+                    logger.warning('var: {} is {} type --> {}'.format(
                         var, type(variables[var]), variables[var]))
                 if type(variables[var]) is dict:
-                    logger.warn('{} is matrix'.format(var))
+                    logger.warning('{} is matrix'.format(var))
 
     # building_for - function to create loop for
     # variable - loop's variable
@@ -82,7 +85,8 @@ class FileBuilder:
     # step - step in loop
     # stop - end value
     # inside - loop body
-    def building_for(self, variable, condition, start, step, stop, inside=''):
+    @staticmethod
+    def building_for(variable, condition, start, step, stop, inside=''):
         logger.info("Building for")
         return f"for({variable} = {start}; {variable} {condition} {stop}; {variable} = {variable} + {step}) {{ \n\t{inside} }}"
 
@@ -90,14 +94,16 @@ class FileBuilder:
     # type - variable type
     # name - variable name
     # value - init value
-    def declaration_variable_with_value(self, type, name, value):
+    @staticmethod
+    def declaration_variable_with_value(type, name, value):
         logger.info("Building var with val")
         return f"{type} {name} = {value}"
 
     # declaration_variable - function to create variable
     # type - variable type
     # name - variable name
-    def declaration_variable(self, type, name):
+    @staticmethod
+    def declaration_variable(type, name):
         logger.info("Building var without val")
         return f"{type} {name}"
 
@@ -106,7 +112,8 @@ class FileBuilder:
     # name - function name
     # typedef - definition typedef
     # param - parameters taken by the function
-    def creating_function(self, returnType, name, typedef='', param=''):
+    @staticmethod
+    def creating_function(returnType, name, typedef='', param=''):
         logger.info("Building func")
         return f"{typedef} {returnType} {name}({param}) {{ "
 
@@ -115,7 +122,8 @@ class FileBuilder:
     # name - array name
     # first_dim - first dimension of the array
     # second_dim - second dimension of the array
-    def creating_two_dimensional_array(self, type, name, first_dim, second_dim):
+    @staticmethod
+    def creating_two_dimensional_array(type, name, first_dim, second_dim):
         logger.info("Building array two dim")
         return f"{type} {name}[{first_dim}][{second_dim}]"
 
@@ -124,14 +132,16 @@ class FileBuilder:
     # name - array name
     # first_dim - first dimension of the array
     # typedef - definition typedef
-    def creating_one_dimensional_array(self, type, name, first_dim, typedef=''):
+    @staticmethod
+    def creating_one_dimensional_array(type, name, first_dim, typedef=''):
         logger.info("Building array one dim")
         return f"{typedef} {type} {name}[{first_dim}]"
 
     # lt_or_gt - function for determining the sign "less than" or "greater than"
     # type - variable type
     # name - variable name
-    def lt_or_gt(self, startValue, endValue):
+    @staticmethod
+    def lt_or_gt(startValue, endValue):
         if startValue < endValue:
             return '<'
         else:
@@ -155,11 +165,13 @@ class FileBuilder:
 
         # int** A = new int*[rows];
         self.file.writelines("\t" + self.declaration_variable_with_value(
-            typesVariables[2], variables[14], self.creating_one_dimensional_array('new', typesVariables[2], variables[11])) + ";\n")
+            typesVariables[2], variables[14],
+            self.creating_one_dimensional_array('new', typesVariables[2], variables[11])) + ";\n")
 
         # A[0] = new int[rows * cols];
         self.file.writelines("  " + self.creating_one_dimensional_array(
-            '', variables[14], 0) + "=" + self.creating_one_dimensional_array('new', typesVariables[0], 'rows*cols') + ";\n")
+            '', variables[14], 0) + "=" + self.creating_one_dimensional_array('new', typesVariables[0],
+                                                                              'rows*cols') + ";\n")
 
         for instr in self.phrase.instructions:
             if isinstance(instr, Constructions):
@@ -169,11 +181,34 @@ class FileBuilder:
                             if isinstance(instr3, Constructions):
                                 pass
                             else:
-                                print(self.building_for(instr.Constr.init['name'], self.lt_or_gt(instr.Constr.init['value'], instr.Constr.end_condition['value']), instr.Constr.init['value'],
-                                                        instr.Constr.increment['inc'], instr.Constr.end_condition['value'],
-                                                        self.building_for(instr2.Constr.init['name'], self.lt_or_gt(instr2.Constr.init['value'], instr2.Constr.end_condition['value']),
-                                                                          instr2.Constr.init['value'], instr2.Constr.increment['inc'], instr2.Constr.end_condition['value'],
-                                                                          self.creating_two_dimensional_array('', instr3['var'], 'N', 'N') + "=" + instr3['val'] + ";\n\t")))
+                                self.file.writelines(
+                                    self.building_for(
+                                        instr.Constr.init['name'],
+                                        self.lt_or_gt(
+                                            instr.Constr.init['value'],
+                                            instr.Constr.end_condition['value']
+                                        ),
+                                        instr.Constr.init['value'],
+                                        instr.Constr.increment['inc'],
+                                        instr.Constr.end_condition['value'],
+                                        self.building_for(
+                                            instr2.Constr.init['name'],
+                                            self.lt_or_gt(
+                                                instr2.Constr.init['value'],
+                                                instr2.Constr.end_condition['value']
+                                            ),
+                                            instr2.Constr.init['value'],
+                                            instr2.Constr.increment['inc'],
+                                            instr2.Constr.end_condition['value'],
+                                            self.creating_two_dimensional_array(
+                                                '',
+                                                instr3['var'],
+                                                'N',
+                                                'N'
+                                            ) + "=" + instr3['val'] + ";\n\t"
+                                        )
+                                    )
+                                )
         # for a in self.phrase.instructions:
         #     while isinstance(a, Constructions):  # or hasattr(a, 'Constr'):
         #         print(dir(a.Constr))
@@ -181,12 +216,22 @@ class FileBuilder:
         #     print(a)
 
         # for (int i = 1; i < rows; ++i) { A[i] = A[i-1] + cols; };
-        self.file.writelines("\n\t" + self.building_for('i', self.lt_or_gt(1, 10), 1, 1, variables[11], self.creating_one_dimensional_array(
-            '', variables[14], 0) + "=" + self.creating_one_dimensional_array('', variables[14], 'i-1') + " + cols;\n\t") + ";\n")
+        self.file.writelines("\n\t" + self.building_for('i', self.lt_or_gt(1, 10), 1, 1, variables[11],
+                                                        self.creating_one_dimensional_array(
+                                                            '', variables[14],
+                                                            0) + "=" + self.creating_one_dimensional_array('',
+                                                                                                           variables[
+                                                                                                               14],
+                                                                                                           'i-1') + " + cols;\n\t") + ";\n")
 
         # for (int i = 0; i < rows; ++i) { for (int j = 0; j < cols; ++j) { A[i][j] = i*cols+j; }; };
         self.file.writelines("\n\t" + self.building_for('x', self.lt_or_gt(1, 10), 1, 2, 10, self.building_for('y', '<',
-                                                                                                               1, 2, 10, self.creating_two_dimensional_array('', 'A', 'N', 'N') + "= i*cols+j;\n\t") + ";\n\t") + ";\n")
+                                                                                                               1, 2, 10,
+                                                                                                               self.creating_two_dimensional_array(
+                                                                                                                   '',
+                                                                                                                   'A',
+                                                                                                                   'N',
+                                                                                                                   'N') + "= i*cols+j;\n\t") + ";\n\t") + ";\n")
         self.building_cuda()
 
     def building_cuda(self):
@@ -195,7 +240,9 @@ class FileBuilder:
         self.file.writelines(mainCudaFunc1)
 
         mainCudaFunc2 = "\t{0}({1}, {2}[0], {3}({4}) * {5}* {6}, {7});\n\n".format(functions[3], variables[13],
-                                                                                   variables[14], keyWords[5], typesVariables[0], variables[11], variables[12], functions[4])
+                                                                                   variables[14], keyWords[5],
+                                                                                   typesVariables[0], variables[11],
+                                                                                   variables[12], functions[4])
         self.file.writelines(mainCudaFunc2)
 
         mainCudaFunc3 = "\t{0} <<{1},{1}>>({2});\n\n".format(
@@ -203,27 +250,27 @@ class FileBuilder:
         self.file.writelines(mainCudaFunc3)
 
         mainCudaFunc4 = "\t{0}({2}[0], {1}, {3}({4}) * {5}* {6}, {7});\n\n}}".format(functions[4], variables[13],
-                                                                                     variables[14], keyWords[5], typesVariables[0], variables[11], variables[12], functions[5])
+                                                                                     variables[14], keyWords[5],
+                                                                                     typesVariables[0], variables[11],
+                                                                                     variables[12], functions[5])
         self.file.writelines(mainCudaFunc4)
 
     def building_headers(self):
-
-        distros_dict = self.phrase.variables
 
         for i, lib in enumerate(libraries):
             if libraries and i != 0:
                 self.file.writelines("{0} {1}\n".format(libraries[0], lib))
         self.file.writelines('\n')
 
-        if distros_dict:
-            for dist in distros_dict:
-                if 'value' in distros_dict[dist]:
+        if self.phrase.variables:
+            for dist in self.phrase.variables:
+                if 'value' in self.phrase.variables[dist]:
                     self.file.writelines("{0} {1} {2}\n".format(
-                        keyWords[0], dist, distros_dict[dist]['value']))
-                if 'size' in distros_dict[dist] :
+                        keyWords[0], dist, self.phrase.variables[dist]['value']))
+                if 'size' in self.phrase.variables[dist]:
                     self.file.write("{0} {1}".format(keyWords[0], dist))
-                    for elem in range(len(distros_dict[dist]['size'])):
-                        self.file.write("[{0}]".format(distros_dict[dist]['size'][elem]))
+                    for elem in range(len(self.phrase.variables[dist]['size'])):
+                        self.file.write("[{0}]".format(self.phrase.variables[dist]['size'][elem]))
             self.file.writelines('\n')
 
         for i, nmsp in enumerate(namespaces):
@@ -232,9 +279,17 @@ class FileBuilder:
         self.file.writelines('\n')
 
         # typedef int arrtype[N];
-        # TODO: review that in future
-        self.file.writelines(self.creating_one_dimensional_array(
-            typesVariables[0], variables[1], variables[0], keyWords[1]) + ";")
+        if self.phrase.variables:
+            for dist in self.phrase.variables:
+                if 'value' in self.phrase.variables[dist]:
+                    self.file.writelines('{};'.format(
+                        self.creating_one_dimensional_array(
+                            self.phrase.variables[dist]['dtype'],
+                            variables[1],
+                            dist,
+                            keyWords[1]
+                        ),
+                    ))
         self.file.writelines('\n')
 
     def building_kernel(self):
@@ -256,7 +311,8 @@ class FileBuilder:
         self.file.writelines(myKernelBody1)
 
         definitionVariable2 = "\t{0} {1} = {2};\n\t{3} {4} = {5};\n\t{6} {7};\n\t".format(
-            typesVariables[0], variables[4], variables[0], typesVariables[0], variables[5], variables[0], typesVariables[0], variables[6])
+            typesVariables[0], variables[4], variables[0], typesVariables[0], variables[5], variables[0],
+            typesVariables[0], variables[6])
         self.file.writelines(definitionVariable2)
 
         myKernelIf = "if({0} >= {1} || {2} >= {3})\n\t\t{4};\n\n\t".format(
